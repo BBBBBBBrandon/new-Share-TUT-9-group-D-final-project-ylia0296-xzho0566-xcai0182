e02834f93f895a1d7d121ff9d4ac0e8f0b4fae39
xiejieyuanzhou <xzho0566@uni.sydney.edu.au>
2024-05-26 22:34:34 +1000

    first revision

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let palette;
 let y = 0;
 
 function preload() {
-  img = loadImage('assets/jpg');
+  img = loadImage('assets/quay.jpg');
 }
 
 function setup() {
@@ -14,6 +14,11 @@ function setup() {
     '#264653', '#2a9d8f',
     '#e9c46a', '#f4a261',
     '#e76f51', '#ff6f61',
+    '#b0824f', '#dd9d51',
+    '#58302e', '#ad342e',
+    '#f73a28', '#3a669c',
+    '#dc7542', '#bd5a42',
+    '#d5b171', '#936c4a', 
   ];
 
   image(img, 0, 0);
@@ -36,10 +41,14 @@ function draw() {
       continue;
     }
 
-  let brushSize = random(1, 2);
-    stroke(paletteColor);
-    strokeWeight(brushSize);
-    point(x + random(-brushSize, brushSize), y + random(-brushSize, brushSize));
+    let brushSize;
+    if (x % 2 === 0) { 
+      brushSize = 3; 
+    } else {
+      brushSize = 1; 
+    }
+    
+    drawBlurredCircle(x, y, brushSize, paletteColor);
   }
 
   y++;
@@ -72,4 +81,19 @@ function getPaletteColor(imgColor) {
   }
 
   return targetColor;
+}
+
+function drawBlurredCircle(x, y, size, color) {
+  noStroke();
+  fill(color);
+
+  let angle = random(TWO_PI);
+  let length = random(size * 0.5, size * 1.5);
+  let dx = cos(angle) * length;
+  let dy = sin(angle) * length;
+  for (let i = 0; i < 5; i++) {
+    let offsetX = random(-size, size);
+    let offsetY = random(-size, size);
+    ellipse(x + dx + offsetX, y + dy + offsetY, size, size);
+  }
 }
