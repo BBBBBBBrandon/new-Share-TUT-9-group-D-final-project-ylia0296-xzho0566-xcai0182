c78b84dbb249af39a8363093a5c6b5a1468e4f3e
xcai0182 <xcai0182@uni.sydney.edu.au>
2024-05-29 06:41:13 +0800

    add new window resieze

--- sketch.js
+++ sketch.js
@@ -1,23 +1,34 @@
 //We need a variable to hold our image
 let img;
+
 let palette;
+
 let y = 0;
-//Let's add a variable for the canvas aspect ratio
+
+//Let's make an object to hold the draw properties of the image
+let imgDrwPrps = {aspect: 0, width: 0, height: 0, xOffset: 0, yOffset: 0};
+
+//And a variable for the canvas aspect ratio
+let canvasAspectRatio = 1200 / 800;
 const originalWidth = 1200;
 const originalHeight = 800;
 let aspectRatio = originalWidth / originalHeight;
 
 //let's load the image from disk
 function preload() {
-  img = loadImage('assets/quay.jpg');
+  img = loadImage('/assets/quay.jpg');
 }
 
 function setup() {
-   //We will make the canvas the same size as the image using its properties
-  calculateCanvasSize();
-  img.resize(width, height);
-
-  //we use Photoshop to find the color
+  //We will make the canvas the same size as the image using its properties
+  let canvasSize = calculateCanvasSize();
+  createCanvas(canvasSize.canvasWidth, canvasSize.canvasHeight);
+  //let's calculate the aspect ratio of the image - this will never change so we only need to do it once
+  img.resize(canvasSize.canvasWidth, canvasSize.canvasHeight);
+  imgDrwPrps.aspect = img.width / img.height;
+  //now let's calculate the draw properties of the image using the function we made
+  calculateImageDrawProps(canvasSize.canvasWidth, canvasSize.canvasHeight);
+  
   palette = [
     '#264653', '#2a9d8f',
     '#e9c46a', '#f4a261',
@@ -36,8 +47,8 @@ function setup() {
 function draw() {
   //brush effect
   for (let x = 0; x < width; x++) {
-    const imgColor = img.get(x, y);
-    
+    const imgColor = img.get(floor(x * (img.width / width)), floor(y * (img.height / height)));
+
     if (imgColor === undefined) {
       console.log(`Undefined color at (${x}, ${y})`);
       continue;
@@ -50,11 +61,11 @@ function draw() {
       continue;
     }
 
-    //Let's add a variable of brushSize
-    let brushSize = (x % 2 === 0) ? 3 : 3;
+    let brushSize = (x % 1 === 0) ? 3 : 3;
     
     drawBlurredCircle(x, y, brushSize, paletteColor);
   }
+
 
   y++;
   if (y >= height) {
@@ -104,21 +115,53 @@ function drawBlurredCircle(x, y, size, color) {
 }
 
 function windowResized() {
-  calculateCanvasSize();
-  img.resize(width, height);
+  let canvasSize = calculateCanvasSize();
+  resizeCanvas(canvasSize.canvasWidth, canvasSize.canvasHeight);
+  calculateImageDrawProps(canvasSize.canvasWidth, canvasSize.canvasHeight);
   y = 0; //Restart palette effect
   loop(); // restart the draw loop
 }
 
 function calculateCanvasSize() {
-   //Calculate the aspect ratio of the canvas
   let canvasWidth = windowWidth;
   let canvasHeight = windowWidth / aspectRatio;
-  //Let the canvas maintain proportions
+  
   if (canvasHeight > windowHeight) {
     canvasHeight = windowHeight;
     canvasWidth = windowHeight * aspectRatio;
   }
 
-  createCanvas(canvasWidth, canvasHeight);
+  return { canvasWidth, canvasHeight };
+}
+
+function calculateImageDrawProps(canvasWidth, canvasHeight) {
+  //Calculate the aspect ratio of the canvas
+  canvasAspectRatio = canvasWidth / canvasHeight;
+  //if the image is wider than the canvas
+  if (imgDrwPrps.aspect > canvasAspectRatio) {
+    //then we will draw the image to the width of the canvas
+    imgDrwPrps.width = canvasWidth;
+    //and calculate the height based on the aspect ratio
+    imgDrwPrps.height = canvasWidth / imgDrwPrps.aspect;
+    imgDrwPrps.yOffset = (canvasHeight - imgDrwPrps.height) / 2;
+    imgDrwPrps.xOffset = 0;
+  } else if (imgDrwPrps.aspect < canvasAspectRatio) {
+    //otherwise we will draw the image to the height of the canvas
+    imgDrwPrps.height = canvasHeight;
+    //and calculate the width based on the aspect ratio
+    imgDrwPrps.width = canvasHeight * imgDrwPrps.aspect;
+    imgDrwPrps.xOffset = (canvasWidth - imgDrwPrps.width) / 2;
+    imgDrwPrps.yOffset = 0;
+  } else {
+    imgDrwPrps.width = canvasWidth;
+    imgDrwPrps.height = canvasHeight;
+    imgDrwPrps.xOffset = 0;
+    imgDrwPrps.yOffset = 0;
+  }
+}
+
+function keyPressed() {
+  if (key == " ") {
+    showPaletteEffect = !showPaletteEffect;
+  }
 }
